51b3b61cff99f333e3a36e24f292270d4b1cadb3
Deep Akabari <deep.akabari@etatvasoft.com>
2024-04-19 10:38:47 +0530

    added transaction

--- src/controllers/Provider/Dashboard/dashboard.controller.ts
+++ src/controllers/Provider/Dashboard/dashboard.controller.ts
@@ -599,16 +599,6 @@ export const viewEncounterForm: Controller = async (req, res) => {
     try {
         const { id } = req.params;
 
-        const exists = await MedicalReport.findOne({
-            where: { requestId: id },
-        });
-        if (!exists) {
-            return res.status(httpCode.BAD_REQUEST).json({
-                status: httpCode.BAD_REQUEST,
-                message: messageConstant.FORM_NOT_FOUND,
-            });
-        }
-
         // Retrieve encounter form data
         const viewEncounterForm = await MedicalReport.findAll({
             attributes: [
