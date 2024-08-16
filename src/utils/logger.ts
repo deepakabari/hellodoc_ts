1e5b51390dc50f94642afb0f6ef670ceb9f0da54
Deep Akabari <deep.akabari@etatvasoft.com>
2024-08-16 16:37:53 +0530

    Linting and formatting

--- src/utils/logger.ts
+++ src/utils/logger.ts
@@ -1,15 +1,15 @@
 import winston from 'winston';
 
 export const logger = winston.createLogger({
-    level: 'info', // Set the desired log level (e.g., 'info', 'error', 'debug')
+    level: 'info', 
     format: winston.format.combine(
         winston.format.colorize(),
-        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }), // Customize timestamp format
+        winston.format.timestamp({ format: 'YYYY-MM-DD HH:mm:ss' }),
         winston.format.printf(({ timestamp, level, message }) => {
             return `${timestamp} [${level}]: ${message}`;
         }),
     ),
     transports: [
-        new winston.transports.Console(), // Log to console (you can add more transports here)
+        new winston.transports.Console(),
     ],
 });
--- src/utils/multerConfig.ts
+++ src/utils/multerConfig.ts
@@ -2,7 +2,7 @@ import multer, { StorageEngine } from 'multer';
 import { Request } from 'express';
 import { existsSync } from 'fs';
 
-const uploadDirectory = './src/public/images'
+const uploadDirectory = './src/public/images';
 export const fileStorage: StorageEngine = multer.diskStorage({
     destination: (req, file, cb) => {
         cb(null, uploadDirectory);
@@ -10,11 +10,11 @@ export const fileStorage: StorageEngine = multer.diskStorage({
     filename: (req, file, cb) => {
         let fileName = file.originalname;
         const extension = fileName.split('.').pop();
-        const baseFileName = fileName.substring(0, fileName.lastIndexOf('.'))
+        const baseFileName = fileName.substring(0, fileName.lastIndexOf('.'));
 
         let counter = 1;
-        while(existsSync(`${uploadDirectory}/${fileName}`)) {
-            fileName = `${baseFileName} (${counter}).${extension}`
+        while (existsSync(`${uploadDirectory}/${fileName}`)) {
+            fileName = `${baseFileName} (${counter}).${extension}`;
             counter++;
         }
         cb(null, fileName);
